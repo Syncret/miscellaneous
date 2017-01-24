69db2f08f416af18348250bb0aa64a5f849c16f7
Syncret <syncret@outlook.com>
2017-01-24 23:35:27 +0800

    dnd helper initial files

---
+++ dndhelper/src/app/characterGenerator/basicform.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector:'character-basicform',
+    templateUrl:'basicform.component.html'
+})
+export class BasicformComponent implements OnInit{
+    constructor(){
+
+    }
+
+    ngOnInit(){
+
+    }
+}

b1473eb61a10a62504ef948f07059f7d70432222
Syncret <syncret@outlook.com>
2017-02-08 23:45:37 +0800

    add strength input

--- dndhelper/src/app/characterGenerator/character.ts
+++ dndhelper/src/app/characterGenerator/character.ts
@@ -5,4 +5,19 @@ export class Character{
     alignment:string;
     religion;string;
     otherReligion:string;
+
+    strength=0;
+    dexterity=0;
+    constitution=0;
+    intelligence=0;
+    wisdom=0;
+    charisma=0;
+
+    calcModifier(value:number):number{
+        return Math.floor(value/2-5);
+    }
+
+    get strengthModifier(){
+        return this.calcModifier(this.strength);
+    }
 }
